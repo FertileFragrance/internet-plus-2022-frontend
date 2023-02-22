7e2430d2f974112f01ca53ab8bb6d14ce0dc302f
Silver-2333 <69996838+Silver-2333@users.noreply.github.com>
2023-02-23 03:40:11 +0800

    feat: 迭代二的基本功能

--- src/components/Check.vue
+++ src/components/Check.vue
@@ -1,5 +1,6 @@
 <template>
     <div class="main">
+        <h2 style="margin-top: 10px" @click="goBack">返回上一界面</h2>
         <Breadcrumb :style="{margin: '24px 0'}">
             <BreadcrumbItem>总体情况</BreadcrumbItem>
             <BreadcrumbItem>违规案例库</BreadcrumbItem>
@@ -103,27 +104,41 @@
             </div>
         </div>
         <div class="detail-container-right" >
-            <h2 style="margin: 15px">相似法规条例</h2>
+            <h2 style="margin: 15px" v-if="list===true">相似法规条例</h2>
+            <h2 style="margin: 15px" v-if="list===false" @click="showBack">返回上一界面</h2>
             <template v-if="list===true">
                 <el-table
-                        :data="tableData"
+                        :data="queryDetailList"
                         height="250"
                         max-height="600px"
                         border
+                        row-class-name="rowClassName"
                         style="width: 100%">
                     <el-table-column
-                            prop="date"
-                            label="日期"
-                            width="180">
+                            prop="ranking"
+                            label="序号"
+                            width="80"
+                            align='center'>
                     </el-table-column>
                     <el-table-column
-                            prop="name"
-                            label="姓名"
-                            width="180">
+                            prop="cosine"
+                            label="匹配度"
+                            width="100"
+                            align='center'>
                     </el-table-column>
                     <el-table-column
-                            prop="address"
-                            label="地址">
+                            prop="content"
+                            label="法规内容"
+                            class-name="content1 content"
+                            >
+                        <template slot-scope="scope">
+                            <el-popover trigger="hover" placement="top">
+                                查看详情
+                                <div slot="reference" class="content1" @click="showDetail(scope.row)">
+                                    {{ scope.row.content }}
+                                </div>
+                            </el-popover>
+                        </template>
                     </el-table-column>
                 </el-table>
                 <el-pagination
@@ -139,11 +154,58 @@
                 </el-pagination>
             </template>
             <template v-else>
-                <el-pager
-                        max-height="600px"
-                        border>
+                <el-descriptions class="margin-top" :column="2" border direction="vertical">
+                    <el-descriptions-item span="2">
+                        <template slot="label">
+                            <i class="el-icon-star-on"></i>
+                            文件名称
+                        </template>{{policyDetail.file}}
 
-                </el-pager>
+                    </el-descriptions-item>
+                    <el-descriptions-item>
+                        <template slot="label">
+                            <i class="el-icon-s-promotion"></i>
+                            章节
+                        </template>
+
+                        {{policyDetail.chapter || '无' }}
+                    </el-descriptions-item>
+                    <el-descriptions-item>
+                        <template slot="label">
+                            <i class="el-icon-s-promotion"></i>
+                            条目
+                        </template>
+
+                        {{policyDetail.article || '无' }}
+
+                    </el-descriptions-item>
+                    <el-descriptions-item >
+                        <template slot="label">
+                            <i class="el-icon-s-promotion"></i>
+                            发布部门
+                        </template>
+
+                        {{policyDetail.department || '无' }}
+
+                    </el-descriptions-item>
+                    <el-descriptions-item>
+                        <template slot="label">
+                            <i class="el-icon-s-promotion"></i>
+                            匹配度
+                        </template>
+                        {{policyDetail.cosine || '无' }}
+
+                    </el-descriptions-item>
+                    <el-descriptions-item   span="2">
+                        <template slot="label">
+                            <i class="el-icon-s-promotion"></i>
+                            内容详情
+                        </template>
+
+                        {{policyDetail.content || '无' }}
+
+                    </el-descriptions-item>
+                </el-descriptions>
             </template>
         </div>
     </div>
@@ -152,81 +214,139 @@
 
 <script>
     // import axios from "axios";
+    import {queryByPenaltyIdAPI,queryByPolicyIdAPI} from "../api/api";
+
     export default {
         data() {
             return {
                 list: true,
-                detail:{
-                    id: "1",
-                    name: "string", //行政处罚名称
-                    number: "string", //行政处罚决定文号
-                    type: "1", //处罚类型//只有"personal","organization"两种
-                    partyName: "string", //被罚当事人名称
-                    responsiblePersonName: "string", //主要负责人姓名
-                    facts: "string1111111111111111111111111111111", //主要违法违规事实（案由）
-                    basis: "string", //行政处罚依据
-                    decision: "string", //行政处罚决定
-                    organName: "string", //行政处罚的机关名称
-                    date: "2021-10-31", //作出处罚决定的日期
-                    status: "0", //状态//只有"0":未发布,"1":已发布两种
-                },
                 tableData: [{
-                    date: '2016-05-03',
-                    name: '王小虎',
-                    address: '上海市普陀区金沙江路 1518 弄'
+                    id: 1,
+                    file: '《代收代付业务管理办法》',
+                    department:  '三农业务部',
+                    article:  '第一条',
+                    content:   '为加强代收代付业务管理，防范业务风险，维护我行、委托人及其他当事人的合法权益，依据《商业银行法》、《商业银行服务价格管理暂行办法》等的有关规定，结合我行实际情况，特制定本办法。',
+                    chapter:   "第一章  总则",
+                    ranking:   1,
+                    cosine:   0.8,
                 }, {
-                    date: '2016-05-02',
-                    name: '王小虎',
-                    address: '上海市普陀区金沙江路 1518 弄'
+                    id: 1,
+                    file: '《代收代付业务管理办法》',
+                    department:  '三农业务部',
+                    article:  '第一条',
+                    content:   '为加强代收代付业务管理，防范业务风险，维护我行、委托人及其他当事人的合法权益，依据《商业银行法》、《商业银行服务价格管理暂行办法》等的有关规定，结合我行实际情况，特制定本办法。',
+                    chapter:   "第一章  总则",
+                    ranking:   1,
+                    cosine:   0.8,
                 }, {
-                    date: '2016-05-04',
-                    name: '王小虎',
-                    address: '上海市普陀区金沙江路 1518 弄'
+                    id: 1,
+                    file: '《代收代付业务管理办法》',
+                    department:  '三农业务部',
+                    article:  '第一条',
+                    content:   '为加强代收代付业务管理，防范业务风险，维护我行、委托人及其他当事人的合法权益，依据《商业银行法》、《商业银行服务价格管理暂行办法》等的有关规定，结合我行实际情况，特制定本办法。',
+                    chapter:   "第一章  总则",
+                    ranking:   1,
+                    cosine:   0.8,
                 }, {
-                    date: '2016-05-01',
-                    name: '王小虎',
-                    address: '上海市普陀区金沙江路 1518 弄'
+                    id: 1,
+                    file: '《代收代付业务管理办法》',
+                    department:  '三农业务部',
+                    article:  '第一条',
+                    content:   '为加强代收代付业务管理，防范业务风险，维护我行、委托人及其他当事人的合法权益，依据《商业银行法》、《商业银行服务价格管理暂行办法》等的有关规定，结合我行实际情况，特制定本办法。',
+                    chapter:   "第一章  总则",
+                    ranking:   1,
+                    cosine:   0.8,
                 }, {
-                    date: '2016-05-08',
-                    name: '王小虎',
-                    address: '上海市普陀区金沙江路 1518 弄'
+                    id: 1,
+                    file: '《代收代付业务管理办法》',
+                    department:  '三农业务部',
+                    article:  '第一条',
+                    content:   '为加强代收代付业务管理，防范业务风险，维护我行、委托人及其他当事人的合法权益，依据《商业银行法》、《商业银行服务价格管理暂行办法》等的有关规定，结合我行实际情况，特制定本办法。',
+                    chapter:   "第一章  总则",
+                    ranking:   1,
+                    cosine:   0.8,
                 }, {
-                    date: '2016-05-06',
-                    name: '王小虎',
-                    address: '上海市普陀区金沙江路 1518 弄'
+                    id: 1,
+                    file: '《代收代付业务管理办法》',
+                    department:  '三农业务部',
+                    article:  '第一条',
+                    content:   '为加强代收代付业务管理，防范业务风险，维护我行、委托人及其他当事人的合法权益，依据《商业银行法》、《商业银行服务价格管理暂行办法》等的有关规定，结合我行实际情况，特制定本办法。',
+                    chapter:   "第一章  总则",
+                    ranking:   1,
+                    cosine:   0.8,
                 }, {
-                    date: '2016-05-07',
-                    name: '王小虎',
-                    address: '上海市普陀区金沙江路 1518 弄'
+                    id: 1,
+                    file: '《代收代付业务管理办法》',
+                    department:  '三农业务部',
+                    article:  '第一条',
+                    content:   '为加强代收代付业务管理，防范业务风险，维护我行、委托人及其他当事人的合法权益，依据《商业银行法》、《商业银行服务价格管理暂行办法》等的有关规定，结合我行实际情况，特制定本办法。',
+                    chapter:   "第一章  总则",
+                    ranking:   1,
+                    cosine:   0.8,
                 }],
                 totalData: [{
-                    date: '2016-05-03',
-                    name: '王小虎',
-                    address: '上海市普陀区金沙江路 1518 弄'
+                    id: 1,
+                    file: '《代收代付业务管理办法》',
+                    department:  '三农业务部',
+                    article:  '第一条',
+                    content:   '为加强代收代付业务管理，防范业务风险，维护我行、委托人及其他当事人的合法权益，依据《商业银行法》、《商业银行服务价格管理暂行办法》等的有关规定，结合我行实际情况，特制定本办法。',
+                    chapter:   "第一章  总则",
+                    ranking:   1,
+                    cosine:   0.8,
                 }, {
-                    date: '2016-05-02',
-                    name: '王小虎',
-                    address: '上海市普陀区金沙江路 1518 弄'
+                    id: 1,
+                    file: '《代收代付业务管理办法》',
+                    department:  '三农业务部',
+                    article:  '第一条',
+                    content:   '为加强代收代付业务管理，防范业务风险，维护我行、委托人及其他当事人的合法权益，依据《商业银行法》、《商业银行服务价格管理暂行办法》等的有关规定，结合我行实际情况，特制定本办法。',
+                    chapter:   "第一章  总则",
+                    ranking:   1,
+                    cosine:   0.8,
                 }, {
-                    date: '2016-05-04',
-                    name: '王小虎',
-                    address: '上海市普陀区金沙江路 1518 弄'
+                    id: 1,
+                    file: '《代收代付业务管理办法》',
+                    department:  '三农业务部',
+                    article:  '第一条',
+                    content:   '为加强代收代付业务管理，防范业务风险，维护我行、委托人及其他当事人的合法权益，依据《商业银行法》、《商业银行服务价格管理暂行办法》等的有关规定，结合我行实际情况，特制定本办法。',
+                    chapter:   "第一章  总则",
+                    ranking:   1,
+                    cosine:   0.8,
                 }, {
-                    date: '2016-05-01',
-                    name: '王小虎',
-                    address: '上海市普陀区金沙江路 1518 弄'
+                    id: 1,
+                    file: '《代收代付业务管理办法》',
+                    department:  '三农业务部',
+                    article:  '第一条',
+                    content:   '为加强代收代付业务管理，防范业务风险，维护我行、委托人及其他当事人的合法权益，依据《商业银行法》、《商业银行服务价格管理暂行办法》等的有关规定，结合我行实际情况，特制定本办法。',
+                    chapter:   "第一章  总则",
+                    ranking:   1,
+                    cosine:   0.8,
                 }, {
-                    date: '2016-05-08',
-                    name: '王小虎',
-                    address: '上海市普陀区金沙江路 1518 弄'
+                    id: 1,
+                    file: '《代收代付业务管理办法》',
+                    department:  '三农业务部',
+                    article:  '第一条',
+                    content:   '为加强代收代付业务管理，防范业务风险，维护我行、委托人及其他当事人的合法权益，依据《商业银行法》、《商业银行服务价格管理暂行办法》等的有关规定，结合我行实际情况，特制定本办法。',
+                    chapter:   "第一章  总则",
+                    ranking:   1,
+                    cosine:   0.8,
                 }, {
-                    date: '2016-05-06',
-                    name: '王小虎',
-                    address: '上海市普陀区金沙江路 1518 弄'
+                    id: 1,
+                    file: '《代收代付业务管理办法》',
+                    department:  '三农业务部',
+                    article:  '第一条',
+                    content:   '为加强代收代付业务管理，防范业务风险，维护我行、委托人及其他当事人的合法权益，依据《商业银行法》、《商业银行服务价格管理暂行办法》等的有关规定，结合我行实际情况，特制定本办法。',
+                    chapter:   "第一章  总则",
+                    ranking:   1,
+                    cosine:   0.8,
                 }, {
-                    date: '2016-05-07',
-                    name: '王小虎',
-                    address: '上海市普陀区金沙江路 1518 弄'
+                    id: 1,
+                    file: '《代收代付业务管理办法》',
+                    department:  '三农业务部',
+                    article:  '第一条',
+                    content:   '为加强代收代付业务管理，防范业务风险，维护我行、委托人及其他当事人的合法权益，依据《商业银行法》、《商业银行服务价格管理暂行办法》等的有关规定，结合我行实际情况，特制定本办法。',
+                    chapter:   "第一章  总则",
+                    ranking:   1,
+                    cosine:   0.8,
                 }],
                 total: 10,
                 pageSize: 1,
@@ -245,9 +365,30 @@
                 date:'',
                 status:''
               },
+                queryList:[],
+                queryDetailList:[],
+                policyDetail:{
+                    id: 1,
+                    file: '《代收代付业务管理办法》',
+                    department:  '三农业务部',
+                    article:  '第一条',
+                    content:   '为加强代收代付业务管理，防范业务风险，维护我行、委托人及其他当事人的合法权益，依据《商业银行法》、《商业银行服务价格管理暂行办法》等的有关规定，结合我行实际情况，特制定本办法。',
+                    chapter:   "第一章  总则",
+                    ranking:   1,
+                    cosine:   0.4,
+                }
             }
         },
         methods: {
+            // eslint-disable-next-line no-unused-vars
+            showDetail(data){
+                this.list = false;
+                this.policyDetail = data;
+            },
+            showBack(){
+                this.list = true;
+                this.policyDetail = {};
+            },
             handleSizeChange(val) {
                 this.pageSize = val;
                 this.currentPage = 1;
@@ -264,18 +405,62 @@
                     this.tableData.push(this.totalData[(this.currentPage-1)*this.pageSize+i])
                 }
             },
-          goBack () {
+            goBack () {
             this.$router.back(-1)
-          }
+            },
+            getQueryList(){
+                const data = this.detailInfo.id;
+                console.log(data);
+                queryByPenaltyIdAPI(data).then((res) => {
+                    if (res.code === "0") {
+                      this.queryList = res.data;
+                    } else {
+                        this.$message.error(res.msg);
+                    }
+                });
+            },
+            getPolicyDetail(id){
+                console.log(id);
+                let re = {};
+                queryByPolicyIdAPI(id).then((res) => {
+                    if (res.code === "0") {
+                        this.re = res.data;
+                    } else {
+                        this.$message.error(res.msg);
+                    }
+                });
+                return re;
+            },
+            getQueryDetailList(){
+                this.queryDetailList = [];
+                this.getQueryList();
+                for (let i = 0; i < this.queryList.size(); i++) {
+                    let res = this.getPolicyDetail(this.queryList[i].innerPolicyId);
+
+                    let policy = {};
+                    policy.id = res.id;
+                    policy.file = res.file;
+                    policy.department = res.file;
+                    policy.article = res.file;
+                    policy.content = res.file;
+                    policy.chapter = res.file;
+                    policy.ranking = this.queryList[i].ranking;
+                    policy.cosine = this.queryList[i].cosine;
+
+                    this.queryDetailList.push(policy);
+                }
+            }
         },
 
       name: "Check",
-
-      created () {
+        mounted() {
+            this.getQueryDetailList();
+        },
+        created () {
         const res = this.$route.query.pid;
          // console.log(res.name)
-        this.detailInfo = res
-        console.log(this.detailInfo)
+        this.detailInfo = res;
+        console.log(this.detailInfo);
       },
 
     }
@@ -315,6 +500,12 @@
     }
     .content{
         margin-left: 10px;
+    }
+    .content1{
+        overflow: hidden;
+        white-space: nowrap;
+        text-overflow: ellipsis;
+
     }
 
 
